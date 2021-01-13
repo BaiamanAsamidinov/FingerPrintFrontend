6009f26f29bb7a8afaa101a45e0cf5c07956f2c5
BaiamanAsamidinov <bayaman.asamidinov1@gmail.com>
2021-01-13 18:51:23 +0600

    First Commit

--- my-file/src/App.js
+++ my-file/src/App.js
@@ -8,9 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Kanat kandaisyn ukam?
-          Jakwysynby?
-
         </p>
         <a
           className="App-link"
